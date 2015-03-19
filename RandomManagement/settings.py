ec9f79f1bbee8e39b3fbbc5fbfb77fb48ed3db6d
unknown <2073036n@BO715-2-05.ad.dcs.gla.ac.uk>
2015-03-19 19:40:09 +0000

    Merging with tasks

--- RandomManagement/settings.py
+++ RandomManagement/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'dajaxice',
-    'dajax',
     'project_management',
 
 )
--- project_management/urls.py
+++ project_management/urls.py
@@ -9,15 +9,11 @@ urlpatterns = patterns('',
         url(r'^dashboard', views.dashboard, name='dashboard'),
         url(r'^addProject', views.addProject, name='newProject'),
         url(r'^new_task/(?P<project_id>[\w\-]+)/$', kris_views.new_task, name='new_task'),
-        url(r'^project/(?P<project_slug>[\w\-]+)/$', views.project, name='project_slug'),
+        url(r'^project/(?P<project_slug>[\w\-]+)/$', views.project, name='project'),
         url(r'^task/(?P<task_id>[\w\-]+)/$', kris_views.task, name="task"),
         url(r'^complete_task/', kris_views.complete_task, name="complete_task"),
         url(r'^profile/', views.profile, name='profile'),
         url(r'^approve_task/(?P<task_id>[\w\-]+)/$', kris_views.approve_task, name="approve_task"),
-
-#----- Konstatin----------
-        url(r'^new_message(?P<task_id>[\w\-]+)/$', kris_views.new_message, name = 'new_message'),
-#-----
         url(r'^project/(?P<project_id>[\w\-]+)/completed_tasks/',
             kris_views.completed_and_approved_tasks, name="completed_and_approved"),
         url(r'^get_tasks/', kris_views.get_offset_task_json, name="offset_task")
